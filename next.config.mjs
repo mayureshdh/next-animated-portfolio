30ec006a590d82da70bac33a92412e366aa17e4c
mayureshdh <mayureshdhamnakar2@gmail.com>
2024-03-03 23:01:15 +0530

    updated code with email

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mayureshdh.github.io",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
